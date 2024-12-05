d3ded9178ba0177006f1d5ac1fa3ad62af51e125
DHDuong2903 <dohuyduong2903@gmail.com>
2024-12-05 15:40:36 +0700

    divide the layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { Experimental_CssVarsProvider as CssVarProvider } from "@mui/material/styles";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+<React.StrictMode>
+    <CssVarProvider theme={theme}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </CssVarProvider>
+  </React.StrictMode>
+);
