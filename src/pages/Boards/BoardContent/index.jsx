58fab7e7cd228229a85f38c2143ca6692730191e
DHDuong2903 <dohuyduong2903@gmail.com>
2024-12-05 16:38:50 +0700

    refactor_project_structure

---
+++ src/pages/Boards/BoardContent/index.jsx
@@ -0,0 +1,18 @@
+import Box from "@mui/material/Box";
+
+const BoardContent = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      Board Content
+    </Box>
+  );
+};
+
+export default BoardContent;
