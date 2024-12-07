6b05edb0700daac87a38c6c6d51850efdbe1c204
DHDuong2903 <dohuyduong2903@gmail.com>
2024-12-07 14:35:03 +0700

    handle_appbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "~/App.jsx";
 import CssBaseline from "@mui/material/CssBaseline";
-import theme from "./theme.js";
+import theme from "~/theme.js";
 
 import { Experimental_CssVarsProvider as CssVarProvider } from "@mui/material/styles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
+  <React.StrictMode>
     <CssVarProvider theme={theme}>
       <CssBaseline>
         <App />
